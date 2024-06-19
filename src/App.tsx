995b5e91b48587c73c9ed93f4cf14e99ac543a14
esy <yesman5307@gmail.com>
2024-06-20 02:16:54 +0900

    add: typescript-adapted theme adjusted to project

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,51 @@
 import styled from "styled-components";
 import Circle from "./Circle";
+import { useState } from "react";
 
-function App() {
+function App1() {
+  const [value, setValue] = useState("");
+  const onChange = (event: React.FormEvent<HTMLInputElement>) => {
+    const {
+      currentTarget: { value },
+    } = event;
+    setValue(value);
+  };
+
+  const onSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    console.log("hello", value);
+  };
+
   return (
+    // <div>
+    //   <Circle bgColor="teal" borderColor="red" />
+    //   <Circle text="im here" bgColor="tomato" />
+    // </div>
     <div>
-      <Circle bgColor="teal" borderColor="red" />
-      <Circle text="im here" bgColor="tomato" />
+      <form onSubmit={onSubmit}>
+        <input
+          value={value}
+          onChange={onChange}
+          type="text"
+          placeholder="username"
+        />
+        <button>Log in</button>
+      </form>
     </div>
   );
 }
+const Container = styled.div`
+  background-color: ${(props) => props.theme.bgColor};
+`;
+const H1 = styled.h1`
+  color: ${(props) => props.theme.textColor};
+`;
 
+function App() {
+  return (
+    <Container>
+      <H1>protected</H1>
+    </Container>
+  );
+}
 export default App;
---
+++ src/theme.ts
@@ -0,0 +1,15 @@
+import { DefaultTheme } from "styled-components/dist/types";
+
+// https://styled-components.com/docs/api#typescript 참고할 것
+
+export const lightTheme: DefaultTheme = {
+  bgColor: "white",
+  textColor: "black",
+  btnColor: "tomato",
+};
+
+export const darkTheme: DefaultTheme = {
+  bgColor: "black",
+  textColor: "white",
+  btnColor: "teal",
+};
